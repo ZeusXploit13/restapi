b9a806292c1ab86b4793fca6db66a8d83f544909
ZeusXploit13 <nugraharizky062@gmail.com>
2025-03-24 14:31:36 +0700

    Update createimage.js

--- public/createimage.js
+++ public/createimage.js
@@ -1,5 +1,8 @@
-const cheerio = require("cheerio")
-const axios = require("axios")
+const express = require("express");
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+const app = express();
 app.use(express.json());
 
 // ChatGPT Endpoint
